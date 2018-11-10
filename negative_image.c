1c840fb72a68c8966d6aaeb3866610b638543bab
andrei828 <andrei.liviu10@gmail.com>
2018-11-10 18:34:31 +0200

    image processing files

---
+++ negative_image.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE * in = fopen("image.bmp", "rb");
+    FILE * out = fopen("modified_image.bmp", "wb");
+
+    int x, y;
+    for (int i = 0; i < 54; i++)
+    {
+        fread(&x, 1, 1, in);
+        fwrite(&x, 1, 1, out);
+    }
+
+    while (fread(&x, 1, 1, in) == 1)
+    {
+        y = 255 - x;
+        fwrite(&y, 1, 1, out);
+    }
+
+    fclose(in);
+    fclose(out);
+}
+
